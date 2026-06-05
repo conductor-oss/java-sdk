847fd241ba93cfda83c1ab8c1e1bdef8e21a94ac
Chris Hagglund <chris.hagglund@orkes.io>
2026-06-05 08:56:24 -0600

    spotless

--- conductor-client/src/main/java/io/orkes/conductor/client/http/TokenRefreshInterceptor.java
+++ conductor-client/src/main/java/io/orkes/conductor/client/http/TokenRefreshInterceptor.java
@@ -19,7 +19,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-
 import okhttp3.Interceptor;
 import okhttp3.Request;
 import okhttp3.Response;
